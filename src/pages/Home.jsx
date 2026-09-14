1aacd01f8e6fa701158a35d9291b86b7fe170982
Ahmed-Assem99 <ahmedassem13@gmail.com>
2026-09-14 14:08:47 +0300

    setting up app env

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='bg-amber-300 text-5xl'>Home Page</div>
+  )
+}
+   
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    open:true
-  }
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+  },
 })
